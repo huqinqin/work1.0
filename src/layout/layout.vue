55d53d1a6c080c6531da5eb4229c14464107ab89
liuyong <liuyong@bullyun.com>
2017-12-21 13:44:11 +0800

    add icons

--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -8,10 +8,10 @@
           <el-col :span="4">
             <el-row height="60px" class="icons" type="flex" justify="end" calss="row-bg">
               <el-col class="user"><span>user</span></el-col>
-              <el-col class="avatar"><a href="#"><i class="icon-lianxiren"></i></a></el-col>
-              <el-col class="contact"><a href="#"><i class="icon-shezhi"></i></a></el-col>
-              <el-col class="setting"><a href="#"><i class="icon-tongzhi"></i></a></el-col>
-              <el-col class="message"><a href="#"><i class="icon-fuwu2"></i>
+              <el-col class="avatar"><a href="#"><i class="iconfont icon-lianxiren"></i></a></el-col>
+              <el-col class="contact"><a href="#"><i class="iconfont icon-shezhi"></i></a></el-col>
+              <el-col class="setting"><a href="#"><i class="iconfont icon-tongzhi"></i></a></el-col>
+              <el-col class="message"><a href="#"><i class="iconfont icon-fuwu2"></i>
                 <!--<div class="message-round">12</div>-->
                 </a>
               </el-col>
@@ -30,42 +30,42 @@
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="2">
-              <template slot="title"><i class="icon-icon--"></i><span>商品</span></template>
+              <template slot="title"><i class="iconfont icon-icon--"></i><span>商品</span></template>
               <el-menu-item-group>
                 <el-menu-item index="2-1">{{$t('name')}}</el-menu-item>
                 <el-menu-item index="2-2">{{$t('menu.order')}}</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="3">
-              <template slot="title"><i class="icon-customer"></i><span>客户</span></template>
+              <template slot="title"><i class="iconfont icon-customer"></i><span>客户</span></template>
               <el-menu-item-group>
                 <el-menu-item index="3-1">{{$t('name')}}</el-menu-item>
                 <el-menu-item index="3-2">{{$t('menu.order')}}</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="4">
-              <template slot="title"><i class="icon-caigou"></i><span>采购</span></template>
+              <template slot="title"><i class="iconfont icon-caigou"></i><span>采购</span></template>
               <el-menu-item-group>
                 <el-menu-item index="4-1">{{$t('name')}}</el-menu-item>
                 <el-menu-item index="4-2">{{$t('menu.order')}}</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="5">
-              <template slot="title"><i class="icon-cangku1"></i><span>仓库</span></template>
+              <template slot="title"><i class="iconfont icon-cangku1"></i><span>仓库</span></template>
               <el-menu-item-group>
                 <el-menu-item index="5-1">{{$t('name')}}</el-menu-item>
                 <el-menu-item index="5-2">{{$t('menu.order')}}</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="6">
-              <template slot="title"><i class="icon-caiwu"></i><span>财务</span></template>
+              <template slot="title"><i class="iconfont icon-caiwu"></i><span>财务</span></template>
               <el-menu-item-group>
                 <el-menu-item index="6-1">{{$t('name')}}</el-menu-item>
                 <el-menu-item index="6-2">{{$t('menu.order')}}</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="7">
-              <template slot="title"><i class="icon-yingxiao"></i><span>营销</span></template>
+              <template slot="title"><i class="iconfont icon-yingxiao"></i><span>营销</span></template>
               <el-menu-item-group>
                 <el-menu-item index="7-1">{{$t('name')}}</el-menu-item>
                 <el-menu-item index="7-2">{{$t('menu.order')}}</el-menu-item>
@@ -77,7 +77,7 @@
           <el-main>
             <lts-content/>
           </el-main>
-          <el-footer>copyright @ 2017 LTS Security Inc. All Rights Reserved</el-footer>
+          <el-footer height="30px">copyright @ 2017 LTS Security Inc. All Rights Reserved</el-footer>
         </el-container>
       </el-container>
     </el-container>
@@ -103,6 +103,7 @@
    }
 </script>
 <style lang="less">
+  @import '//at.alicdn.com/t/font_516449_wdlmisobbd2njyvi.css';
   *{margin:0;padding:0;box-sizing: border-box;}
   ul,ol{list-style: none;}
   a{color:inherit;text-decoration: none;}
@@ -130,14 +131,9 @@
           color: #777;
           margin-right: -20px;
           i::before{
-            border:1px solid red;
+            position: relative;
             width:20px;
             height: 20px;
-          }
-          img{
-            vertical-align: middle;
-            width: 24px;
-            height: 24px;
           }
           // 消息右上角的红点
           .message-round{
@@ -148,7 +144,6 @@
             line-height: 24px;
             height: 24px;
             color:white;
-            border:1px solid red;
             border-radius: 50%;
             background: #e8293f;
           }
@@ -156,31 +151,39 @@
       }
 
       .el-aside{
+        color:#666666;
         height:100%;
         margin-top: 10px;
-        .el-submenu__title{
-          overflow: hidden;
-          line-height: 70px;
-          height:70px;
-          span{
-            font-size: 18px;
-            margin-left: 30px;
+        .el-submenu{
+          .el-submenu__title{
+            border-left: 4px solid rgba(0,0,0,0);
+            overflow: hidden;
+            margin:5px 0;
+            line-height: 60px;
+            height:60px;
+            span{
+              font-size: 18px;
+              margin-left: 30px;
+            }
+            i{
+              width:22px;
+              height: 22px;
+              border:1px solid green;
+            }
+            i::before{
+              border:1px solid red;
+            }
           }
+          border-bottom:1px solid #e5e5e5;
         }
-        .el-menu{
-          padding-left: 20px;
-        }
-        .el-submenu.opened{
+        .el-submenu.is-opened{
           .el-submenu__title{
             border-left: 4px solid #ce2127;
             color:#ce2127;
-            .el-icon-arrow-down{
-              color:inherit;
-            }
           }
          }
         .el-menu-item{
-          padding:0;
+          margin-left: 20px;
         }
         img{
           width:21px;
@@ -190,23 +193,12 @@
         }
         .el-menu-item{
           font-size: 16px;
-          width:180px;
         }
-        .is-opened{
-          .el-submenu__title{
-            color:red;
-          }
-          i{
-            color:red;
-          }
-        }
-      }
-      .el-footer{
-        text-align: center;
       }
       .el-footer{
         font-size: 14px;
-        line-height: 30px;
+        text-align: center;
+        color:#999999;
       }
     }
   }
