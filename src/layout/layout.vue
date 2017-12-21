d874c6c9bc9bd346edbf19e4699a05b7d0603aae
liuyong <liuyong@bullyun.com>
2017-12-21 15:20:19 +0800

    some refresh

--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -12,7 +12,7 @@
               <el-col class="contact"><a href="#"><i class="iconfont icon-fuwu2"></i></a></el-col>
               <el-col class="setting"><a href="#"><i class="iconfont icon-shezhi"></i></a></el-col>
               <el-col class="message"><a href="#"><i class="iconfont icon-tongzhi"></i>
-                <div class="message-round">12</div>
+                <div class="message-round">n+</div>
                 </a>
               </el-col>
             </el-row>
@@ -23,14 +23,39 @@
         <el-aside width="180px">
           <el-menu class="el-menu-vertical" :default-openeds="['1']" :unique-opened="isUniqueOpened" active-text-color="#ce2127">
             <el-submenu index="1">
-              <template slot="title" color="red"><i class="iconfont icon-dingdan"></i>订单</template>
+              <template slot="title"><i class="iconfont icon-dingdan"></i>订单</template>
               <el-menu-item index="1-1" width="140px">{{$t('name')}}</el-menu-item>
               <el-menu-item index="1-2">{{$t('menu.order')}}</el-menu-item>
             </el-submenu>
             <el-submenu index="2">
-              <template slot="title" color="red"><i class="iconfont icon-dingdan"></i>订单</template>
-              <el-menu-item index="1-1">{{$t('name')}}</el-menu-item>
-              <el-menu-item index="1-2">{{$t('menu.order')}}</el-menu-item>
+              <template slot="title"><i class="iconfont icon-icon--"></i>商品</template>
+              <el-menu-item index="2-1">{{$t('name')}}</el-menu-item>
+              <el-menu-item index="2-2">{{$t('menu.order')}}</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><i class="iconfont icon-customer"></i>客户</template>
+              <el-menu-item index="3-1">{{$t('name')}}</el-menu-item>
+              <el-menu-item index="3-2">{{$t('menu.order')}}</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title"><i class="iconfont icon-caigou"></i>采购</template>
+              <el-menu-item index="4-1">{{$t('name')}}</el-menu-item>
+              <el-menu-item index="4-2">{{$t('menu.order')}}</el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title"><i class="iconfont icon-cangku1"></i>仓库</template>
+              <el-menu-item index="5-1">{{$t('name')}}</el-menu-item>
+              <el-menu-item index="5-2">{{$t('menu.order')}}</el-menu-item>
+            </el-submenu>
+            <el-submenu index="6">
+              <template slot="title"><i class="iconfont icon-caiwu"></i>财务</template>
+              <el-menu-item index="6-1">{{$t('name')}}</el-menu-item>
+              <el-menu-item index="6-2">{{$t('menu.order')}}</el-menu-item>
+            </el-submenu>
+            <el-submenu index="7">
+              <template slot="title"><i class="iconfont icon-yingxiao"></i>营销</template>
+              <el-menu-item index="7-1">{{$t('name')}}</el-menu-item>
+              <el-menu-item index="7-2">{{$t('menu.order')}}</el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
@@ -93,7 +118,7 @@
         z-index: 10;
         line-height: 60px;
         height: 60px;
-        box-shadow: 0 3px 10px #f2f2f2;
+        box-shadow: 0 3px 15px #f2f2f2;
         .el-row{
           height:100%;
         }
@@ -107,16 +132,17 @@
           color: #777;
           margin-right: -20px;
           .iconfont{
-            font-size: 24px;
+            font-size: 20px;
           }
           // 消息右上角的红点
           .message-round{
             position: relative;
             top:-50px;
             left:8px;
-            width:24px;
-            line-height: 24px;
+            width:22px;
+            line-height: 22px;
             color:white;
+            text-align: center;
             border-radius: 50%;
             background: #e8293f;
           }
@@ -126,23 +152,32 @@
       .el-aside{
         color:#666666;
         height:100%;
-        margin-top: 10px;
         }
         .el-submenu{
-          .el-submenu_title{
+          .el-submenu__title{
+            margin-left: 4px;
             line-height: 70px;
-            height: 70px;
+            height:70px;
+            border-bottom:1px solid #e5e5e5;
           }
           .el-menu-item{
             min-width:100px;
+            margin-left: 14px;
+          }
+        }
+        .el-submenu.is-opened{
+          color: red;
+          .el-submenu__title{
+            margin-left: 0px;
+            border-left:4px solid #ce2127;
           }
         }
         .iconfont{
-          font-size: 24px;
+          font-size: 20px;
           margin-right: 10px;
         }
         .el-icon-arrow-down{
-          font-size: 16px;
+          font-size: 14px;
         }
       }
       .el-footer{
