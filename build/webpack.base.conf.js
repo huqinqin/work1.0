e2640a425b6a604ae2c554dbb50ba200b4202567
王海明 <wanghaiming@500mi.com>
2018-01-02 13:12:42 +0800

    fixed

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -26,6 +26,7 @@ module.exports = {
       '@'      : resolve('src'),
       'config' : resolve('src') + '/config',
       'layout' : resolve('src') + '/layout/LTS_Layout_1.0.0/layout.vue',
+
       'const'  : resolve('src') + '/const',
       'ltsutil': resolve('src') + '/utils',
       'ui'     : resolve('src') + '/common'
