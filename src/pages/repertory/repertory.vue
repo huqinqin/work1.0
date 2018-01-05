8b6921f0a31a0cfc1e604365848a0abfe1a3167b
王海明 <wanghaiming@500mi.com>
2018-01-05 14:01:25 +0800

    编辑商品

--- src/pages/repertory/repertory.vue
+++ src/pages/repertory/repertory.vue
@@ -1,25 +1,24 @@
 <template>
     <div>
-        <router-link to="manage"><el-button>库存管理</el-button></router-link>
-        <router-link to="list"><el-button>库存明细</el-button></router-link>
         <router-view>
-
         </router-view>
-
     </div>
+
 </template>
 
 <script>
-    import RepertoryList from './components/repertoryList'
-    import RepertoryManage from './components/repertoryManage'
     export default {
         name: "repertory",
-        components: {RepertoryManage, RepertoryList},
         data(){
             return{
+              activeName : 'manage'
             }
         },
-        methods:{}
+        methods:{
+          handleClick(tab){
+            this.$route.push({name : tab.name})
+          },
+        }
   }
 </script>
 
