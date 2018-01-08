d3a7d6427443f70d2acd1cde38026837c65e14fe
黄键华 <huangjh@asiainfo.com>
2018-01-08 11:06:24 +0800

    trade

--- src/pages/trade/components/tradeReverse.vue
+++ src/pages/trade/components/tradeReverse.vue
@@ -1,42 +1,71 @@
 <template>
   <div class="trade-reverse">
-    <!--查询条件-->
-    <el-form ref="form" :model="form" :rules="rules" label-position="left">
-      <el-form-item label="订单编号" label-width="110px" prop="bizName">
-        <el-input v-model="form.bizName"></el-input>
-      </el-form-item>
-      <el-form-item label="店铺名称" label-width="110px" prop="bizEname">
-        <el-input v-model="form.bizEname"></el-input>
-      </el-form-item>
-      <el-form-item label="商品名称" label-width="110px" prop="bizEname">
-        <el-input v-model="form.bizEname"></el-input>
-      </el-form-item>
-      <el-form-item label="创建时间" label-width="110px" prop="bizEname">
-        <el-input v-model="form.bizEname"></el-input>
-        -
-        <el-input v-model="form.bizEname"></el-input>
-      </el-form-item>
-      <el-form-item label="" label-width="110px">
-        <el-button type="primary" @click="submit">登记</el-button>
-        <el-button type="error" @click="resetForm('form')">清空</el-button>
-      </el-form-item>
-    </el-form>
-
+    <lts-search-form
+            @get-from="getParameter"
+            :form-fileds="form.formFileds"
+            :form-inlines="form.formInline"></lts-search-form>
     <lts-table :t-api="api" :t-form="form.formInline" :t-table="table" :t-pagination="pagination"
                @menuClick="oper"></lts-table>
   </div>
 </template>
 <script>
+  import {ltsTable, ltsSearchForm} from 'ui'
   import {request} from 'ltsutil'
-  import segmentsService from '@/services/SegmentsService'
 
   export default {
-    props  : {},
-    name   : 'tradeReverse',
+    props     : {},
+    name      : 'tradeReverse',
+    components: {ltsTable, ltsSearchForm},
     data() {
       return {
-        api       : '',
-        form      : {},
+        api       : {
+          method   : '',
+          bizparams: {
+            user_id: ''
+          }
+        },
+        form      : {
+          formFileds: {
+            'search':{
+              test:{
+                tid: {
+                  label          : "订单编号",
+                  type           : "input",
+                  bindValue      : "tid",
+                  width          : '120px'
+                },
+                shop_name: {
+                  label          : "店铺名称",
+                  type           : "input",
+                  bindValue      : "tid",
+                  width          : '120px'
+                },
+                item_name: {
+                  label          : "商品名称",
+                  type           : "input",
+                  bindValue      : "tid",
+                  width          : '120px'
+                },
+                cdate: {
+                  label          : "创建时间",
+                  type           : "date",
+                  bindValue      : "tid",
+                  width          : '120px'
+                },
+                search    : {"bindValue": "搜索", "type": "submitbutton"},
+              }
+            }
+          },
+          formInline: {
+            page             : 1,
+            page_size        : 100,
+            order_by         : 'id',
+            tid       : '',
+            // custom_type : '',
+            //需要从处理结果另外带回来的参数 存这里 默认返回一条对象。若返回多个。自己
+            callbackParameter: {},
+          }
+        },
         pagination: {
           page    : 1,
           pagesize: 10,
@@ -49,12 +78,12 @@
           tableDataForm: 'api', // json
           tableField   : {
             '订单编号/商品信息': {'value': 'biz_ename', 'type': 'text'},
-            '规格': {'value': 'biz_name', 'type': 'text'},
-            '订单数量/金额': {'value': 'contact', 'type': 'text'},
-            '退货数量/金额' : {'value': 'status_cname', 'type': 'text'},
-            '申请时间/付款时间' : {'value': 'status_cname', 'type': 'text'},
-            '状态' : {'value': 'status_cname', 'type': 'text'},
-            '功能' : {
+            '规格'       : {'value': 'biz_name', 'type': 'text'},
+            '订单数量/金额'  : {'value': 'contact', 'type': 'text'},
+            '退货数量/金额'  : {'value': 'mount', 'type': 'text'},
+            '申请时间/付款时间': {'value': 'cdate', 'type': 'text'},
+            '状态'       : {'value': 'state', 'type': 'text'},
+            '功能'       : {
               'value'   : '',
               'type'    : 'menu',
               'width'   : '200',
@@ -75,19 +104,13 @@
         }
       }
     },
-    methods: {
+    methods   : {
       submit() {
-        let formData    = Object.assign({}, this.form);
-        let addSegments = segmentsService.addSegmentsItem(formData)
-        addSegments.then((data) => {
-          console.log('success')
-        }, (msg) => {
-          console.log(msg)
-          this.$ltsMessage.show({type: 'error', message: msg.error_message})
-        })
       },
       oper() {
 
+      },
+      getParameter() {
       }
     },
     mounted() {
