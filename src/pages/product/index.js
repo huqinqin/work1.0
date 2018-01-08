6ad58101e5dc3d4ee6da58508da08a01ba6c93be
王海明 <wanghaiming@500mi.com>
2018-01-08 12:36:17 +0800

    修改produt 名称

---
+++ src/pages/product/index.js
@@ -0,0 +1,4 @@
+import Lib from '@/common/js/'
+import App from './product.vue'
+import routerView from './router/'
+Lib(App, routerView)
--- src/pages/products/index.js
+++
@@ -1,4 +0,0 @@
-import Lib from '@/common/js/'
-import App from '@/pages/products/product.vue'
-import routerView from '@/pages/products/router/index.js'
-Lib(App, routerView)
