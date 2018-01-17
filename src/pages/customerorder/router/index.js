47431c30979b8f4382d7c5548b2c28a5af1ec8a5
王海明 <wanghaiming@500mi.com>
2018-01-17 21:14:15 +0800

    fixed

--- src/pages/customerorder/router/index.js
+++ src/pages/customerorder/router/index.js
@@ -1,22 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Cart from '../components/cart.vue'
-import Order from '../components/order.vue'
-
+import beforePay from '../components/beforePay.vue'
+import settle from '../components/settle.vue'
+import finish from '../components/finish.vue'
+import order from '../components/order.vue'
 Vue.use(Router)
-
 export default new Router({
-  mode: 'history',
   routes: [
-    {
-      path: '/customerorder/cart',
-      name: 'cart',
-      component: Cart
-    },
-    {
-      path: '/customerorder/order',
-      name: 'order',
-      component: Order
-    }
+      {
+          path: '/',
+          name: 'order',
+          component: order
+      },
+      {
+            path: '/cart',
+            name: 'cart',
+            component: Cart
+      },
+      {
+            path: '/settle',
+            name: 'settle',
+            component: settle
+      },
+      {
+            path: '/beforePay',
+            name: 'beforePay',
+            component: beforePay
+      },
+      {
+          path: '/finish',
+          name: 'finish',
+          component: finish
+      }
   ]
 })
